7eb9c277697fe97bcff01a3d318f17a86bdd78df
kewook55 <bono4023@gmail.com>
2020-08-16 23:25:26 +0900

    BOJ 문제풀이

--- 2014-소수의곱(구현, Heap).cpp
+++ 2014-소수의곱(구현, Heap).cpp
@@ -51,3 +51,50 @@ int main(void) {
 
 	return 0;
 }
+
+
+/*
+중복을 제거하는데 map을 사용하지 않는 방법
+min heap을 유지하면서 heap의 top원소를 N번 꺼낸다.
+꺼내면서 입력받은 소수들의 곱을 top원소와 곱해주면서 갱신해주면 가능하다.
+하지만 중복이 생기는데 이 중복을 방지해주기 위해 이미 곱해줫던것을 제거해주면 가능하다.
+
+https://mygumi.tistory.com/183 => 이 블로그에 있는 글을 참고했다.
+*/
+#include<iostream>
+#include<functional>
+#include<queue>
+
+#define long long long
+using namespace std;
+
+int N, K, ar[101];
+priority_queue<long, vector<long>, greater<long>> pq;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> K >> N;
+	for (int i = 0; i < K; i++) {
+		cin >> ar[i];
+		pq.push(ar[i]);
+	}
+
+    //N번 꺼내줘야하니 N-1번만 꺼내주면서 곱하여 갱신해준다.
+	for (int t = 1; t < N; t++) {
+		long top = pq.top();
+		pq.pop();
+
+		for (int i = 0; i < K; i++) {
+			long next = top * ar[i];
+			pq.push(next);
+
+            //중복제거
+			if (!(top % ar[i]))break;
+		}
+	}
+    //N번째 꺼냄.
+	cout << pq.top();
+	return 0;
+}
